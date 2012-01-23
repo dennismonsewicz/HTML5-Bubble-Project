fae4269fc44de0f145aac234b6bf257fbcd1a2cd
Dennis Monsewicz <dennismonsewicz@gmail.com>
2012-01-23 11:14:53 -0600

    uploading modified JS library for showing how to use oCanvas JS Lib with creating elements for the HTML5 canvas and adding in a collision detection algorithm

--- js/scripts.js
+++ js/scripts.js
@@ -1,47 +1,90 @@
-var context, canvas, clientOpts;
-var x = 100;
-var y = 200;
-var dx = 5;
-var dy = 5; //.2 is pretty slow
+// EXTENDING OBJECTS
+	Array.prototype.min = function(array) {
+		return Math.min.apply(Math, array);
+	}
+	
+	Array.prototype.max = function(array) {
+		return Math.max.apply(Math, array)
+	}
+//
 
-function drawCircle() {
-    // sliding up
-			//     if (y > target) {
-			// y -= dy;
-			//     }
-			
-		if(clientOpts) {
-			if(clientOpts.cy < y)
-				y -= dy;
-			else
-				y += dy;
+// GLOBALS
+    var xmlData = '<?xml version="1.0" encoding="UTF-8"?><root name="CompanyName"><projects><project name="Project1"></project><project name="Project2"></project><project name="Project3"></project></projects></root>'
+
+    var xmlObj = []
+		var xmlDoc, xml;
+    var padding = 15
+    var canvas = oCanvas.create({
+        canvas: '#myCanvas'
+    })
+    var c_width = canvas.width
+    var c_height = canvas.height
+
+    var logo = canvas.display.ellipse({
+        x: c_width / 2,
+        y: c_height / 3,
+        radius: 80,
+        fill: '#d15851'
+    })
+
+		var rectObj = function(){
+			this.x =  0;
+			this.y =  0;
+			this.width =  100;
+			this.height = 100;
+			this.size = this.width + this.height; //this would equate to a circles radius if dealing with circles
+			this.fillerText =  null;
+			this.fillRect = function(hexVal){
+				if(!hexVal)
+					return '#'+'0123456789abcdef'.split('').map(function(v,i,a){
+	            return i>5 ? null : a[Math.floor(Math.random()*16)] }).join('')
+				else
+					return hexVal
+					
+			};
+			this.drawRect = function(){
+				return canvas.display.rectangle({
+					width: this.width,
+					height: this.height,
+					fill: this.fillRect(),
+					x: this.x,
+					y: this.y
+				})
+			};
+			this.checkCollisions = function(objToCheck) {
+				var centerA = { x: this.x+(this.size/2), y: this.y+(this.size/2) };
+				var centerB = { x:objToCheck.x+(objToCheck.size/2), y: objToCheck.y+(objToCheck.size/2) };
+				var distance = Math.sqrt(((centerB.x-centerA.x)*(centerB.x-centerA.x) + (centerB.y-centerA.y)*(centerB.y-centerA.y)));
 				
-			if(clientOpts.cx < x)
-				x -= dx;
-			else
-				x += dx;
-		}
-		
-		context.clearRect(0, 0, canvas.width, canvas.height);
-		context.beginPath()
-		context.fillStyle = "#0000ff";
-		context.arc(x, y, 20, 0, Math.PI * 2, true);
-		context.fill();
-		context.closePath();
-}
-
-window.onload = function () {
-    // get canvas, and context once
-    canvas = document.getElementById('myCanvas');
-    context = canvas.getContext('2d');
-
-    // set target y position
-    canvas.onclick = function (e) {
-			clientOpts = {
-				cy: e.clientY,
-				cx: e.clientX
+				if(distance < (this.size+objToCheck.size)) {
+					objToCheck.x = this.x - (canvas.width/4)
+				}
 			}
+		}
+
+    canvas.addChild(logo)
+
+    var parseXML = function() {
+        xmlDoc = $.parseXML(xmlData)
+        xml = $(xmlDoc)
+
+        xml.find('project').each(function(i){
+					xmlObj[i] = new rectObj()
+					xmlObj[i].fillerText = $(this).attr('name')
+					xmlObj[i].x = (logo.x + logo.radius * Math.cos((360*Math.PI) / (i + 1)) + padding) + ((xmlObj[i].width / 2) + (i+1));
+					xmlObj[i].y = (logo.y + logo.radius) + padding;
+        });
+				
+				for(i = 0; i < xmlObj.length; i++) {
+					for(a = i+1; a < xmlObj.length; a++) {
+						xmlObj[i].checkCollisions(xmlObj[a])
+					}
+					canvas.addChild(xmlObj[i].drawRect())
+				}
     }
-    // 30fps
-    setInterval(drawCircle, (1000 / 30));
-}
+
+//
+
+$(document).ready(function(){
+    parseXML()
+})
